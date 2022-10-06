a69b3f7e04dd37621374ead084f302050757cabb
MandoDB <77023822+MandoDB@users.noreply.github.com>
2022-10-06 15:26:24 +0200

    Add files via upload

--- exercice1.js
+++ exercice1.js
@@ -19,13 +19,19 @@ var articles = [
 function ArrayUpdate() {
     var list = document.getElementById('listeCourse');
     var restoreList = document.getElementById('article');
+    var itemSwap1 = document.getElementById('item1');
+    var itemSwap2 = document.getElementById('item2');
 
     list.innerHTML = ""
     restoreList.innerHTML = ""
+    itemSwap1.innerHTML = ""
+    itemSwap2.innerHTML = ""
 
     for (let i = 0; i < articles.length; i++) {
         list.innerHTML += `<li id="li_${articles[i]}">${articles[i]} <button id="hide_${articles[i]}">Hide</button> <button id="del_${articles[i]}">x</button></li>`;
         restoreList.innerHTML += `<option value="${articles[i]}">`
+        itemSwap1.innerHTML += `<option value="${articles[i]}">`
+        itemSwap2.innerHTML += `<option value="${articles[i]}">`
     }
 }
 ArrayUpdate()
@@ -37,20 +43,23 @@ function hideDelArticle(event) {
 
     var splitedValue = el.split("_");
     var value = splitedValue[0]
+    var li = splitedValue[1]
     switch (value) {
         case "del":
             if (!articles.includes(splitedValue[1])) return;
-            var listEl = document.getElementById(`li_${splitedValue[1]}`);
-            listEl.remove()
-            articles.pop(splitedValue[1])
+            var listEl = document.getElementById(`li_${li}`);
+            var index = articles.indexOf(li);
+            articles.splice(index, 1)
+            listEl.remove(li)
             ArrayUpdate()
             break;
         case "hide":
-            if (!articles.includes(splitedValue[1])) return;
-            var listEl = document.getElementById(`li_${splitedValue[1]}`);
+            if (!articles.includes(li)) return;
+            var listEl = document.getElementById(`li_${li}`);
             listEl.style.display = "none";
-        default: 
-        console.log("none")
+            break;
+        default:
+            console.log("none")
     }
 }
 
@@ -68,10 +77,22 @@ document.getElementById('submitElement').addEventListener('click', function () {
 
 document.getElementById('restoreSubmit').addEventListener('click', function () {
     var selectedRestore = document.getElementById('selectedOption');
-    console.log(selectedRestore.value)
 
     var showEl = document.getElementById(`li_${selectedRestore.value}`);
     if (showEl.style.display != "none") return alert("This article is not hide")
     showEl.style.display = "";
     selectedRestore.value = "";
+})
+
+document.getElementById('swapItemSubmit').addEventListener('click', function () {
+    var swapItem1 = document.getElementById('swapItem1');
+    var swapItem2 = document.getElementById('swapItem2');
+    var index1 = articles.indexOf(swapItem1.value);
+    var index2 = articles.indexOf(swapItem2.value);
+
+    articles[index1] = swapItem2.value;
+    articles[index2] = swapItem1.value;
+    swapItem1.value = ""
+    swapItem2.value = ""
+    ArrayUpdate()
 })
